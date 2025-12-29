e1a2956bda9e7ae239549500aeedf13180192d8f
Perry Benyella <pebenyella@microsoft.com>
2025-12-28 23:45:46 -0800

    Add Products DbSet to AppDbContext
    Added using directive for Product model and introduced a Products DbSet property in AppDbContext to enable EF management of Product entities.

--- ProductsManagement/Data/AppDbContext.cs
+++ ProductsManagement/Data/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductsManagement.Models;
 
 namespace ProductsManagement.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<Product> Products => Set<Product>();
     }
 }
