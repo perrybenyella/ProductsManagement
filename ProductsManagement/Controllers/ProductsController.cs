bb61a76ac5f61cae5ae9389974810520c10d8a79
Perry Benyella <pebenyella@microsoft.com>
2025-12-28 22:27:49 -0800

    Add GET endpoint to return hardcoded list of products
    Added a GetProducts HTTP GET method to ProductsController that returns a hardcoded list of products (Id, Name, Price) as a JSON response with HTTP 200 status.

--- ProductsManagement/Controllers/ProductsController.cs
+++ ProductsManagement/Controllers/ProductsController.cs
@@ -7,5 +7,16 @@ namespace ProductsManagement.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetProducts() // Get method that gets the list of products
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Laptop", Price = 999.99 },
+                new { Id = 2, Name = "Smartphone", Price = 499.99 },
+                new { Id = 3, Name = "Tablet", Price = 299.99 }
+            };
+            return Ok(products); // Returns a list of products with HTTP 200 status
+        }
     }
 }
