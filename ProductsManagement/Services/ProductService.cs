def9cfada34bce2f687c57525e89768049b52dce
Perry Benyella <pebenyella@microsoft.com>
2025-12-29 19:20:22 -0800

    Implement IProductService interface in ProductService
    Added method stubs for product operations (add, delete, get, update) to ProductService, each throwing NotImplementedException. Also imported ProductsManagement.Models namespace.

--- ProductsManagement/Services/ProductService.cs
+++ ProductsManagement/Services/ProductService.cs
@@ -1,6 +1,32 @@
-﻿namespace ProductsManagement.Services
+﻿using ProductsManagement.Models;
+
+namespace ProductsManagement.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        public void AddedProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletableProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
