b19f8265eb7f1b28ca71d0b2d0fe593047834574
Perry Benyella <pebenyella@microsoft.com>
2025-12-29 19:15:49 -0800

    Add CRUD methods to IProductService interface
    Expanded IProductService with methods for retrieving, adding, updating, and deleting products. Added using directive for ProductsManagement.Models to support the Product type. This update enables basic CRUD operations for product management.

--- ProductsManagement/Services/IProductService.cs
+++ ProductsManagement/Services/IProductService.cs
@@ -1,6 +1,13 @@
-﻿namespace ProductsManagement.Services
+﻿using ProductsManagement.Models;
+
+namespace ProductsManagement.Services
 {
     public interface IProductService
     {
+        IEnumerable<Product>  GetAllProducts();
+        Product? GetProductById(int id);
+        void AddedProduct(Product product);
+        void UpdateProduct(int id, Product product);
+        void DeletableProduct(Product product);
     }
 }
